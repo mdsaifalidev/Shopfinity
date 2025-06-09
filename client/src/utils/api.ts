e1ef7f48d9581c6c39fe48c7dc2e618122c01fda
mdsaifalidev <saifmd9536@gmail.com>
2025-06-09 11:58:46 +0530

    Refactor login form validation logic and update API_BASE_URL to use NEXT_PUBLIC environment variable

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,4 +1,6 @@
-export const API_BASE_URL = process.env.API_BASE_URL;
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
+
+console.log("API_BASE_URL:", API_BASE_URL);
 
 export const API_ROUTES = {
   AUTH: `${API_BASE_URL}/api/auth`,

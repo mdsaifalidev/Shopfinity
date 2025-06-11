0b84e1955bea7b99f5e9c162e0d66eda073d9218
mdsaifalidev <saifmd9536@gmail.com>
2025-06-11 11:05:45 +0530

    Add comments to clarify cookie expiration times in setTokens function

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -27,14 +27,14 @@ async function setTokens(
     httpOnly: true,
     secure: true,
     sameSite: "none",
-    maxAge: 60 * 60 * 1000,
+    maxAge: 60 * 60 * 1000, // 1 hour
     path: "/",
   })
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: true,
     sameSite: "none",
-    maxAge: 7 * 24 * 60 * 60,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     path: "/",
   })
 }
